e124343b60d8d911f99ede8c525ebec3866c59b6
Gianluca Consoli <gianluca.consoli@newteam.it>
2025-02-18 11:56:56 +0100

    Initialized all files

---
+++ VisualConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace variables 
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            
+            string firstName;
+            string lastName;
+            string age;
+
+            Console.WriteLine("Enter your name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            age = Console.ReadLine(); 
+
+            // con concatenazione di stringhe usando l'operatore +
+            //string message = "These are the data you entered: " + firstName + ", " + lastName + ", " + age + "";
+            // con interpolazione di stringhe
+            string message = $"Your name is {firstName}, your last name is {lastName} and you are {age} years old"; // questo è il miglior metodo da usare
+            // con formattazione di stringhe, i numeri definiscono i segnaposto
+            message = String.Format("Your name is {0}, your last name is {1} and you are {2} years old", firstName, lastName, age);
+
+            Console.WriteLine(message);
+            
+            
+        }
+
+    }
+
+}
+
